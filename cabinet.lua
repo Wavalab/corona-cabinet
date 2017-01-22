5bafe7bf71ed0119702a54c14c14029563c87f83
John Chin-Jew <jchinjew@gmail.com>
2017-01-22 15:09:48 -0800

    formatting

--- cabinet.lua
+++ cabinet.lua
@@ -1,10 +1,7 @@
---[[
-  Cabinet v1.0 | MIT | github.com/wavalab/corona-cabinet
-  Save/retrieve Tables, Numbers, Strings & Booleans via persistent JSON file.
-  WARNING: Cabinet is built for Corona SDK & requires Corona's JSON library!
---]]
 local j = require "json"
-local cabinet, fname, defdir = {}, "c_.json", system.DocumentsDirectory
+local cabinet = {}
+local fname, defdir = "c_.json", system.DocumentsDirectory
+
 local rj = function(fname, dir)
   local t_, f = {}, io.open(system.pathForFile(fname, dir), "r")
   if not f then return end
@@ -12,26 +9,33 @@ local rj = function(fname, dir)
   io.close(f)
   return t_
 end
+
 local wj = function(t_, fname, dir)
   local f = io.open(system.pathForFile(fname, dir), "w")
   if not f then return end
   f:write(j.encode(t_))
   io.close(f)
 end
+
 local t = rj(fname, defdir) or {}
+
 cabinet.set = function(k, v)
   t[k] = v
   wj(t, fname, defdir)
 end
+
 cabinet.get = function(k)
   return t[k]
 end
+
 cabinet.del = function(k)
   t[k] = nil
   wj(t, fname, defdir)
 end
+
 cabinet.clr = function()
   t = {}
   wj(t, fname, defdir)
 end
+
 return cabinet
